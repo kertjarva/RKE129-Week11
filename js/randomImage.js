49e798558b539d906afa4ab12aa9188abaf96e10
Kert <kert.jarva@tktk.ee>
2024-04-15 11:01:58 +0300

    Add week11 code

---
+++ js/randomImage.js
@@ -0,0 +1,12 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector('img');
+const images = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"];
+
+myButton.addEventListener('click', changeImage); 
+
+function changeImage(){
+    let randomIndex = Math.floor(Math.random() * images.length);
+    console.log('changeImage');
+    image.src = "images/" + images[randomIndex]
+
+}
